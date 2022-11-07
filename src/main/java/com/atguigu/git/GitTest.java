43bbb6f4cecc10ad864844724742fe4593861ee4
huangjun <2408131608@qq.com>
2022-11-08 00:13:13 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("git test4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
