bb995c6bd9e3554ad5845bde44c633a1c2e164db
huangjun <2408131608@qq.com>
2022-11-07 23:01:17 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test2");
+        System.out.println("git test3");
     }
 }
