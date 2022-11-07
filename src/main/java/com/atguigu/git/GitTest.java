a3f61ce4cfb06ac236211a13f690fd16bfa56a47
huangjun <2408131608@qq.com>
2022-11-07 23:13:13 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("git test");
         System.out.println("git test2");
         System.out.println("git test3");
+        System.out.println("git test4");
     }
 }
