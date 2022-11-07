077a7f1a7b9cc71051ec3369dedc503ae82f35e9
huangjun <2408131608@qq.com>
2022-11-07 23:00:15 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test2");
     }
 }
